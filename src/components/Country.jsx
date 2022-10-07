cebfbce167e0105b0ee126d36233b5ffc12f61a1
tayjay1019 <57647217+tayjay1019@users.noreply.github.com>
2022-10-06 20:24:53 -0500

    finished V1

---
+++ src/components/Country.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Country extends Component {
+  state = {
+    name: 'United States',
+    gold: 0,
+  }
+  handleIncrement = () => this.setState({ gold: this.state.gold + 1 });
+  render() { 
+    return (
+      <div className="country">
+        <div className="name">
+        { this.state.name }
+        </div>
+        <div className="medals">
+          gold medals: { this.state.gold }
+          <button onClick={ this.handleIncrement }>+</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Country;
