2fdf5f67db4b6c9446e79313010fa7f797c6115b
tayjay1019 <57647217+tayjay1019@users.noreply.github.com>
2022-10-06 20:28:36 -0500

    added v2

--- src/App.js
+++ src/App.js
@@ -7,10 +7,20 @@ import Country from './components/Country';
 import './App.css';
 
 class App extends Component {
-  state = {  }
+  state = {
+    countries: [
+      { id: 1, name: 'United States', gold: 2 },
+      { id: 2, name: 'China', gold: 3 },
+      { id: 3, name: 'Germany', gold: 0 },
+    ]
+  }
   render() { 
     return (
-      <Country />
+      <React.Fragment>
+        { this.state.countries.map(country => 
+          <Country key={ country.id } country={ country } />
+        )}
+      </React.Fragment>
     );
   }
 }
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 
 class Country extends Component {
   state = {
-    name: 'United States',
-    gold: 0,
+    name: this.props.country.name,
+    gold: this.props.country.gold,
   }
   handleIncrement = () => this.setState({ gold: this.state.gold + 1 });
+  handleDecrement = () => this.setState({ gold: this.state.gold - 1 });
   render() { 
     return (
       <div className="country">
@@ -15,7 +16,9 @@ class Country extends Component {
         <div className="medals">
           gold medals: { this.state.gold }
           <button onClick={ this.handleIncrement }>+</button>
+          <button disabled={ this.state.gold === 0 } onClick={ this.handleDecrement }>-</button>
         </div>
+        <hr />
       </div>
     );
   }
